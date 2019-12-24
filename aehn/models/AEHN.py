!!! Fix the dtime broadcast ERROR in AEHN model.

aehn/models/AEHN.py
@@ -31,6 +31,9 @@ class AEHN(BaseModel):
         _, loss, pred_types, pred_time = sess.run([self.train_op, self.loss, self.pred_types, self.pred_time],
                                                   feed_dict=fd)
 
+        density = sess.run([self.density], feed_dict=fd)
+        print(density)
+
         # shape -> [batch_size, max_len - 1]
         preds = {
             'types': pred_types[:, :-1],
@@ -169,7 +172,7 @@ class AEHN(BaseModel):
             target_dtimes = tf.concat([self.dtimes_seq[:, 1:], self.dtimes_seq[:, :1]], axis=-1)
 
             # compute lambdas
-            target_elapses = base_elapses + target_dtimes[:, None, :, None]
+            target_elapses = base_elapses + target_dtimes[:, :, None, None]
             # [batch_size, max_len, process_dim]
             lambdas = self.compute_lambda(mus, alphas, deltas, target_elapses)
 
@@ -183,57 +186,60 @@ class AEHN(BaseModel):
                 alphas_trans = tf.transpose(alphas, perm=[1, 0, 2, 3])[:, :, None]
                 deltas_trans = tf.transpose(deltas, perm=[1, 0, 2, 3])[:, :, None]
                 base_elapses_trans = tf.transpose(base_elapses, perm=[1, 0, 2, 3])[:, :, None]
-                scan_elems = (
-                    mus_trans,
-                    alphas_trans,
-                    deltas_trans,
-                    base_elapses_trans
-                )
 
                 # sample lambdas for loss.
                 dtimes_loss_samples = tf.linspace(start=0.0,
                                                   stop=1.0,
                                                   num=self.n_loss_integral_sample)
-                # [batch_size, n_sample, max_len]
-                dtimes_loss_samples = target_dtimes[:, None, :] * dtimes_loss_samples[None, :, None]
-
+                # [batch_size, max_len, n_sample]
+                dtimes_loss_samples = target_dtimes[:, :, None] * dtimes_loss_samples[None, None, :]
                 # loop over max_len
                 loss_scan_initializer = create_tensor([batch_size, self.n_loss_integral_sample, self.process_dim], 0.0)
 
                 # [batch_size, max_len, n_loss_integral_sample, process_dim]
                 lambdas_loss_samples = self.sample_lambda_by_scan(
-                    self.get_compute_lambda_forward_fn(dtimes_loss_samples[:, :, :, None]),
-                    scan_elems,
+                    self.get_compute_lambda_forward_fn(),
+                    [
+                        mus_trans,
+                        alphas_trans,
+                        deltas_trans,
+                        base_elapses_trans,
+                        swap_axes(dtimes_loss_samples[:, :, :, None, None], 0, 1)
+                    ],
                     loss_scan_initializer
                 )
 
                 # sample lambdas for prediction.
 
-                # [batch_size, n_sample, max_len]
+                # [batch_size, max_len, n_sample]
                 dtimes_pred_samples = tf.linspace(start=0.0,
                                                   stop=self.max_time_pred,
-                                                  num=self.n_pred_integral_sample)[None, :, None]
+                                                  num=self.n_pred_integral_sample)[None, None, :]
                 # use loop to avoid memory explode
                 # loop over max_len
                 pred_scan_initializer = create_tensor([batch_size, self.n_pred_integral_sample, self.process_dim], 0.0)
 
                 # [batch_size, max_len, n_pred_integral_sample, process_dim]
                 lambdas_pred_samples = self.sample_lambda_by_scan(
-                    self.get_compute_lambda_forward_fn(dtimes_pred_samples[:, :, :, None]),
-                    scan_elems,
+                    self.get_compute_lambda_forward_fn(),
+                    [
+                        mus_trans,
+                        alphas_trans,
+                        deltas_trans,
+                        base_elapses_trans,
+                        swap_axes(dtimes_pred_samples[:, :, :, None, None], 0, 1)
+                    ],
                     pred_scan_initializer
                 )
-
-                return lambdas, lambdas_loss_samples, tf.transpose(dtimes_loss_samples, perm=[0, 2, 1]), \
-                       lambdas_pred_samples, tf.transpose(dtimes_pred_samples, perm=[0, 2, 1])
+                return lambdas, lambdas_loss_samples, dtimes_loss_samples, lambdas_pred_samples, dtimes_pred_samples
             else:
                 return lambdas, None, None, None, None
 
-    def get_compute_lambda_forward_fn(self, elapse_bias):
+    def get_compute_lambda_forward_fn(self, ):
         compute_lambda_fn = self.compute_lambda
 
         def forward_fn(acc, item):
-            mu, alpha, delta, elapse = item
+            mu, alpha, delta, elapse, elapse_bias = item
             return compute_lambda_fn(mu, alpha, delta, elapse + elapse_bias)
 
         return forward_fn
@@ -262,6 +268,7 @@ class AEHN(BaseModel):
         right_term = tf.reduce_sum(alpha * tf.exp(-delta * elapse), axis=-2)
         # shape -> [..., hidden_dim]
         imply_lambdas = left_term + right_term
+        # imply_lambdas = right_term
 
         return tf.nn.softplus(tensordot(imply_lambdas, lambda_w) + lambda_b)
 

aehn/models/base_model.py
@@ -74,36 +74,6 @@ class BaseModel(object):
 
         return pred_type_logits, pred_time
 
-    def loglikelihood_inference(self, lambdas_pred_samples, dtimes_pred_samples):
-        """ Predict the type and time from intensity function over infinite time.
-        :param lambdas_pred_samples: [batch_size, max_len, n_pred_sample, process_dim]
-        :param dtimes_pred_samples: [batch_size, max_len, n_pred_sample]
-        :return:
-        """
-        with tf.variable_scope('intensity_inference'):
-            # compute density
-            # [batch_size, max_len, n_pred_sample]
-            lambdas_total_samples = tf.reduce_sum(lambdas_pred_samples, axis=-1)
-            # [batch_size, max_len, n_pred_sample]
-            integral_samples = self.get_acc_rect_integral(lambdas_total_samples,
-                                                          self.max_time_pred / self.n_pred_integral_sample)
-            # [batch_size, max_len, n_pred_sample]
-            density_samples = lambdas_total_samples * tf.exp(-integral_samples)
-
-            # compute time prediction
-            # [batch_size, max_len]
-            pred_times = self.trapezium_integral(density_samples * dtimes_pred_samples, dtimes_pred_samples)
-
-            # compute type prediction
-            # [batch_size, max_len, n_pred_sample, process_dim]
-            type_ratio_samples = lambdas_pred_samples / lambdas_total_samples[:, :, :, None]
-
-            # [batch_size, max_len, process_dim]
-            pred_type_logits = self.trapezium_integral(type_ratio_samples * density_samples[:, :, :, None],
-                                                       dtimes_pred_samples[:, :, :, None])
-
-        return pred_type_logits, pred_times
-
     def shuffle_hybrid_loss(self, pred_type_logits, pred_times):
         """ Calculate the total loss of all points in the sequence by using the latter point as the label predicted by
         the previous point.
@@ -125,7 +95,7 @@ class BaseModel(object):
             pred_type_proba = tf.nn.softmax(pred_type_logits, axis=-1) + 1e-8
 
             # (batch_size, max_len - 1)
-            cross_entropy = tf.reduce_sum(- tf.log(pred_type_proba) * type_label, axis=-1)
+            cross_entropy = tf.reduce_sum(-tf.log(pred_type_proba) * type_label, axis=-1)
             type_loss = tf.reduce_mean(tf.boolean_mask(cross_entropy, seq_mask))
 
             dtimes_pred = pred_times[:, :-1]
@@ -135,6 +105,36 @@ class BaseModel(object):
             time_loss = tf.reduce_mean(tf.abs(time_diff))
 
             return type_loss + time_loss
+
+    def loglikelihood_inference(self, lambdas_pred_samples, dtimes_pred_samples):
+        """ Predict the type and time from intensity function over infinite time.
+        :param lambdas_pred_samples: [batch_size, max_len, n_pred_sample, process_dim]
+        :param dtimes_pred_samples: [batch_size, max_len, n_pred_sample]
+        :return:
+        """
+        with tf.variable_scope('intensity_inference'):
+            # compute density
+            # [batch_size, max_len, n_pred_sample]
+            lambdas_total_samples = tf.reduce_sum(lambdas_pred_samples, axis=-1)
+            # [batch_size, max_len, n_pred_sample]
+            integral_samples = self.get_acc_rect_integral(lambdas_total_samples,
+                                                          self.max_time_pred / self.n_pred_integral_sample)
+            # [batch_size, max_len, n_pred_sample]
+            density_samples = lambdas_total_samples * tf.exp(-integral_samples)
+            self.density = tf.reduce_mean(tf.reduce_sum(density_samples, axis=-1) * self.max_time_pred / self.n_pred_integral_sample)
+            # compute time prediction
+            # [batch_size, max_len]
+            pred_times = self.trapezium_integral(density_samples * dtimes_pred_samples, dtimes_pred_samples)
+
+            # compute type prediction
+            # [batch_size, max_len, n_pred_sample, process_dim]
+            type_ratio_samples = lambdas_pred_samples / lambdas_total_samples[:, :, :, None]
+
+            # [batch_size, max_len, process_dim]
+            pred_type_logits = self.trapezium_integral(type_ratio_samples * density_samples[:, :, :, None],
+                                                       dtimes_pred_samples[:, :, :, None])
+
+        return pred_type_logits, pred_times
 
     def shuffle_loglikelihood_loss(self, lambdas, lambdas_loss_samples, dtimes_loss_samples):
         """
@@ -165,7 +165,7 @@ class BaseModel(object):
 
             # shape -> [batch_size, max_len - 1]
             target_lambdas = tf.reduce_sum(lambdas * seq_event_onehot, axis=-1)
-            target_lambdas_masked = tf.boolean_mask(target_lambdas, target_lambdas > 0)
+            target_lambdas_masked = tf.boolean_mask(target_lambdas, seq_zero_mask > 0)
             term_1 = tf.reduce_sum(tf.log(target_lambdas_masked))
 
             # shape -> [batch_size, max_len - 1]
@@ -187,7 +187,7 @@ class BaseModel(object):
         return integral
 
     def trapezium_integral(self, values, dtimes):
-        """ Trapezium integral over dim-1 (samples dim).
+        """ Trapezium integral over dim-2 (samples dim).
         :param values: [batch_size, None, n_samples, ...]
         :param dtimes: the shape is same as values  or float
         :return: [batch_size, None, ...]